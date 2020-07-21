6859fb9da78555b734a3dd03608f8c69133c6020
Tsahi Barshevsky <38860203+tsahiBarshevsky@users.noreply.github.com>
2020-07-21 16:45:54 +0300

    Adding some changes of my own

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // find the element
 const progressBar = document.querySelector('#progress');
+const backToTop = document.querySelector('#backToTop');
 
 // find the distance from top and change progress bar design
 document.onscroll = () =>
@@ -7,4 +8,8 @@ document.onscroll = () =>
     const distanceTop = window.pageYOffset;
     const pageHeigh = document.body.scrollHeight - window.innerHeight;
     progressBar.style.width = (distanceTop / pageHeigh) * 100 + '%';
+    if (distanceTop >= 500)
+        backToTop.style.display = "block";
+    else
+        backToTop.style.display = "none";
 }
