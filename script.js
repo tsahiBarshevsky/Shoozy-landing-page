9798894f937caf4e66d3558aca71b73c74bf2a6c
Tsahi Barshevsky <38860203+tsahiBarshevsky@users.noreply.github.com>
2020-07-23 17:29:22 +0300

    Adding pop up message

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ document.onscroll = () =>
     else
         backToTop.style.display = "none";
 }
+
+//Pop up meassage
+var modal = document.getElementById("myModal");  //Get the modal
+var link = document.getElementById("modalLink"); //Get the button that opens the modal
+var span = document.getElementsByClassName("close")[0]; //Get the <span> element that closes the modal
+link.onclick = function() //When the user clicks on the button, open the modal
+{
+    modal.style.display = "block";
+}
+span.onclick = function() //When the user clicks on <span> (x), close the modal
+{ 
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) // When the user clicks anywhere outside of the modal, close it
+{
+    if (event.target == modal)
+        modal.style.display = "none";
+}
